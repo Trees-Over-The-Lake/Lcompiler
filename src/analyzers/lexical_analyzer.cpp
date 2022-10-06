9739e216e6574a4d9545e00d2790a1805660dce5
ThiagoHN <thiagohnogueira01@gmail.com>
2022-10-05 23:56:12 -0300

    [feat] Implementação parcial do analisador lexico

--- src/analyzers/lexical_analyzer.cpp
+++ src/analyzers/lexical_analyzer.cpp
@@ -6,6 +6,8 @@
 #include"../error_manager/cerror_manager.cpp"
 #include<cstdint>
 
+typedef std::string::const_iterator iterator;
+
 class LexicalAnalyzer {
     private:
         LFile file_to_analyze;
@@ -18,6 +20,17 @@ class LexicalAnalyzer {
     public:
         LexicalAnalyzer(LFile* file);
         ~LexicalAnalyzer();
+        char estado0(const char c, iterator* it);
+        char estado2(const char c, iterator* it);
+        char estado3(const char c, iterator* it);
+        char estado4(const char c, iterator* it);
+        char estado5(const char c, iterator* it);
+        char estado7(const char c, iterator* it);
+        char estado8(const char c, iterator* it);
+        char estado9(const char c, iterator* it);
+        char estado10(const char c, iterator* it);
+        char estado11(const char c, iterator* it);
+        char estado12(const char c, iterator* it);
 
         void analyze();
 };
@@ -29,6 +42,134 @@ LexicalAnalyzer::LexicalAnalyzer(LFile* file) {
 }
 
 LexicalAnalyzer::~LexicalAnalyzer() {}
+
+char LexicalAnalyzer::estado0(const char c, iterator* it) {
+    if(c == ' ' || c == '\n') {
+        curr_state = 0;
+    }
+    else if(c == '!') {
+        curr_state = 5;
+    }
+    else if(c == '<' || c == '>') {
+        curr_state = 7;
+    }
+    else if(c == '\'') {
+        curr_state = 9;
+    }
+    else if(c == '_' || std::isalpha(c)) {
+        curr_state = 12;
+    }
+    else if(c == '=' || c == '%' || c == '(' || c == ')' || c == '*' || c == '+' || c == ',' || c == '-' || c == ';' || c == '[' || c == ']') {
+        curr_state = 1;
+    }
+    else if(c == '0') {
+        curr_state = 6;
+    }
+    else if(std::isdigit(c) && c != '0') {
+        curr_state = 18;
+    }
+    else if(c == '.') {
+        curr_state = 13;
+    }
+    else if(c == '"') {
+        curr_state = 11;
+    }
+    else if(c == ':') {
+        curr_state = 8;
+    }
+    else if(c == '/') {
+        curr_state = 2;
+    }
+    else {
+        curr_state = 1;
+    }
+
+    return c;
+}
+
+char LexicalAnalyzer::estado2(const char c, iterator* it) {
+    if(c == '*') {
+        curr_state = 3;
+    }
+    else {
+        --*it;
+        curr_state = 1;
+    }
+}
+
+char LexicalAnalyzer::estado3(const char c, iterator* it) {
+    if(c == '*') {
+        curr_state = 4;
+    }
+    else {
+        curr_state = 3;
+    }
+}
+
+char LexicalAnalyzer::estado4(const char c, iterator* it) {
+    if(c == '*') {
+        curr_state = 4;
+    }
+    else if (c == '/'){
+        curr_state = 0;
+    }
+    else {
+        curr_state = 3;
+    }
+}
+
+char LexicalAnalyzer::estado5(const char c, iterator* it) {
+    if (c == '=') {
+        curr_state = 1;
+    }
+}
+
+char LexicalAnalyzer::estado7(const char c, iterator* it) {
+    if (c == '=') {
+        curr_state = 1;
+    }
+    else {
+        --*it;
+        curr_state = 1;
+    }
+}
+
+char LexicalAnalyzer::estado8(const char c, iterator* it) {
+    if (c == '=') {
+        curr_state = 1;
+    }
+}
+
+char LexicalAnalyzer::estado9(const char c, iterator* it) {
+    if (std::isalnum(c)) {
+        curr_state = 10;
+    }
+}
+
+char LexicalAnalyzer::estado10(const char c, iterator* it) {
+    if (c == '\'') {
+        curr_state = 1;
+    }
+}
+
+char LexicalAnalyzer::estado11(const char c, iterator* it) {
+    if (std::isalnum(c)) {
+        curr_state = 11;
+    }
+    else if (c == '"'){
+        curr_state = 1;
+    }
+}
+
+char LexicalAnalyzer::estado12(const char c, iterator* it) {
+    if (std::isdigit(c) || std::isalpha(c) || c == '_') {
+        curr_state = 12;
+    }
+    else {
+        --*it;        
+        curr_state = 1;
+    }
+}
 
 // Função do Analisador Léxico.
 void LexicalAnalyzer::analyze()
@@ -47,11 +188,53 @@ void LexicalAnalyzer::analyze()
         std::string line = curr_line.second;
         int line_number  = curr_line.first;
 
-        for (char const &curr_char : line) {
+        for (auto it = line.cbegin(); it != line.cend(); ++it) {
+            char curr_char = *it;
+
             if(!this->symbol_table.is_character_valid(curr_char)) 
                 throw_compiler_error(CErrorType::CaractereInvalido, 
                                     {std::to_string(line_number)});
-            
+            switch (curr_state)
+            {
+                case 0:
+                    lexeme += estado0(curr_char,&it);
+                break;
+                case 2:
+                    lexeme += estado2(curr_char,&it);
+                break;
+                case 3:
+                    lexeme += estado3(curr_char,&it);
+                break;
+                case 4:
+                    lexeme += estado4(curr_char,&it);
+                break;
+                case 5:
+                    lexeme += estado5(curr_char,&it);
+                break;
+                case 7:
+                    lexeme += estado7(curr_char,&it);
+                break;
+                case 11:
+                    lexeme += estado11(curr_char,&it);
+                break;
+                case 8:
+                    lexeme += estado8(curr_char,&it);
+                break;
+                case 12:
+                    lexeme += estado12(curr_char,&it);
+                break;
+                case 9:
+                    lexeme += estado9(curr_char,&it);
+                break;
+                case 10:
+                    lexeme += estado10(curr_char,&it);
+                break;
+
+
+                default:
+                
+                break;
+            }
         }
     }
 }               
