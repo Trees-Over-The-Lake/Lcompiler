7c6873873655df5ea2a4e824d96c070442794b3f
Lucas Saliba <lucassaliba10@gmail.com>
2022-10-11 15:37:39 -0300

    implementing grammar

--- src/analyzers/parser.cpp
+++ src/analyzers/parser.cpp
@@ -1,5 +1,21 @@
 this->symbol_table = SymbolTable();
 
+// S -> {(A | E)}* fim_arquivo
+void symbol_S()
+{
+    while (READ_TOKEN.lexeme != "fim_arquivo")
+    {
+        if (READ_TOKEN.lexeme == "char" || READ_TOKEN.lexeme == "float" || READ_TOKEN.lexeme == "string" || READ_TOKEN.lexeme == "boolean" || READ_TOKEN.lexeme == "int" || READ_TOKEN.lexeme == "const")
+        {
+            symbol_A();
+        }
+        else
+        {
+            symbol_E();
+        }
+    }
 
+    match_token("fim_arquivo");
+}
 
 
